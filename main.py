b2aa187c83b8a154f7c494d62213a54ac6d0e0bd
Volodymyr Mashchyts <vmashchyts@gmail.com>
2021-11-09 22:38:20 -0800

    fixed indexing in propfinder, added search to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from funcs import *
+from propfinder import *
 
 def main():
    text = get_text('static_data.txt')
@@ -6,6 +7,9 @@ def main():
    propeller_list = objectify(data)
    for propeller in propeller_list:
       print(propeller.thrust_list)
+
+   optpropout = prop_finder(propeller_list, 6)
+   print(optpropout[2].power_list,optpropout[2].thrust_list)
 
 if __name__ == "__main__":
    main()
--- propfinder.py
+++ propfinder.py
@@ -1,29 +1,30 @@
 from typing import List, Any
 
 
-def prop_finder(prop_array,t):
+def prop_finder(prop_array, t):
     # Inputs: prop_array-array of propeller object as defined in propeller class
     pvals = [[], [], []]  # 0 - indices of the props, 1-index of T array, 2 - Power value
     i_prop = 0
     for prop in prop_array:
-        for i in range(len(prop.T)-2):
-            t1 = prop.T[i]
-            t2 = prop.T[i+1]
+        for i in range(len(prop.thrust_list)-2):
+            t1 = prop.thrust_list[i]
+            t2 = prop.thrust_list[i+1]
 
             if (t1 <= t < t2) or (t1 > t >= t2):
                 # making an array of indecies and power values to minimize later
                 pvals[0].append(i_prop)
                 pvals[1].append(i)
-                pval = prop.P[i]
+                pval = prop.power_list[i+1]
                 pvals[2].append(pval)
 
         i_prop += 1
 
     # Finding the optimal propeller
-    p_opt = min(pvals[3])
-    i_inpopt  = pvals[3].index(p_opt)
-    i_optprop = pvals[0][i_inpopt]
-    i_tval    = pvals[1][i_optprop]
-    opt_prop  = prop_array[i_optprop]
+    p_opt = min(pvals[2])
+    i_inpopt  = pvals[2].index(p_opt) # Index of the optimal propeller in p_opt
+    i_optprop = pvals[0][i_inpopt] # Index of the optimal prop in prop_list
 
-    return [i_tval, p_opt, opt_prop]
+    i_inprop  = pvals[1][i_optprop] # Index of the thrust value in thrust list
+    opt_prop  = prop_array[i_optprop] # Optimal prop objects
+
+    return [i_inprop, p_opt, opt_prop]
